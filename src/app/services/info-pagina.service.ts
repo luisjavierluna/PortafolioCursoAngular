ffc595a303b5faa90709ca18edac8fb0b0dce340
luisjavierluna <javierlunaluis@gmail.com>
2023-04-15 17:42:33 -0600

    33. Carga de la información de Firebase a nuestra página

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,17 +10,33 @@ export class InfoPaginaService {
   info: InfoPagina = {}
   cargada = false
 
-  constructor(private http: HttpClient) {
-    // console.log('Servicio de infoPagina listo')
+  equipo: any[] = []
 
+  constructor(private http: HttpClient) {
+
+    this.cargarInfo()
+    this.cargarEquipo()
+  }
+
+  private cargarInfo() {
     // Leer el archivo JSON
-     this.http.get('assets/data/data-pagina.json')
-      .subscribe({
-        next: (resp: InfoPagina) => {
-          this.cargada = true
-          this.info = resp
-          console.log(resp)
-        } 
-      })
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe({
+      next: (resp: InfoPagina) => {
+        this.cargada = true
+        this.info = resp
+      } 
+    })
+  }
+
+  private cargarEquipo() {
+    // Leer el archivo JSON
+    this.http.get('https://angular-hml-default-rtdb.firebaseio.com/.json')
+    .subscribe({
+      next: (resp: any) => {
+        this.equipo = resp
+        console.log(resp)
+      } 
+    })
   }
 }
