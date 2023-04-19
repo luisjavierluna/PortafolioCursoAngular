cf9a1785ae7b354bc82da5ceac4307eedebd8b16
luisjavierluna <javierlunaluis@gmail.com>
2023-04-18 20:16:19 -0600

    45. Servicio para obtener el producto

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductoDescripcion } from 'src/app/interfaces/producto-descripcion.interface';
+import { ProductosService } from 'src/app/services/productos.service';
 
 @Component({
   selector: 'app-item',
@@ -8,12 +10,19 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ItemComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    public productosService: ProductosService) { }
 
   ngOnInit(): void {
     this.route.params
       .subscribe(parametros => {
-        console.log(parametros['id'])
+        this.productosService.getProducto(parametros['id'])
+          .subscribe( (producto: ProductoDescripcion) => {
+
+            console.log(producto)
+
+          })
       })
   }
 
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Producto } from '../interfaces/producto.interface';
+import { ProductoDescripcion } from '../interfaces/producto-descripcion.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -17,9 +18,12 @@ export class ProductosService {
   private cargarProductos() {
     this.http.get<any[]>('https://angular-hml-default-rtdb.firebaseio.com/productos_idx.json')
       .subscribe((resp:Producto[]) => {
-        console.log(resp)
         this.productos = resp
         this.cargando = false
       })
+  }
+
+  getProducto(id:string) {
+    return this.http.get<ProductoDescripcion>(`https://angular-hml-default-rtdb.firebaseio.com/productos/${id}.json`)
   }
 }
