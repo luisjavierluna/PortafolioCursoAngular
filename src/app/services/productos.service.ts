a430699d748b4fa091129c0b8c345444bc5443b4
luisjavierluna <javierlunaluis@gmail.com>
2023-04-19 23:43:29 -0600

    50. Lógica del proceso de carga y filtro

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -17,11 +17,16 @@ export class ProductosService {
   }
 
   private cargarProductos() {
-    this.http.get<any[]>('https://angular-hml-default-rtdb.firebaseio.com/productos_idx.json')
-      .subscribe((resp:Producto[]) => {
-        this.productos = resp
-        this.cargando = false
-      })
+
+    return new Promise<void>( (resolve, reject ) => {
+
+      this.http.get<Producto[]>('https://angular-hml-default-rtdb.firebaseio.com/productos_idx.json')
+        .subscribe((resp:Producto[]) => {
+          this.productos = resp
+          this.cargando = false
+          resolve()
+        })
+    })
   }
 
   getProducto(id:string) {
@@ -30,10 +35,28 @@ export class ProductosService {
 
   buscarProducto(termino: string) {
 
-    this.productosFiltrado = this.productos.filter(producto => {
-      return true
-    })
+    if (this.productos.length === 0) {
+      this.cargarProductos().then(()=> {
+        this.filtrarProductos(termino)
+      })
+    } else {
+      this.filtrarProductos(termino)
+    }
 
-    console.log(this.productosFiltrado)
+  }
+  
+  private filtrarProductos(termino:string) {
+    this.productosFiltrado = []
+    
+    termino = termino.toLocaleLowerCase()
+
+    this.productos.forEach( prod => {
+
+      const tituloLower = prod.titulo.toLocaleLowerCase()
+
+      if(prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0) {
+        this.productosFiltrado.push(prod)
+      }
+    })
   }
 }
